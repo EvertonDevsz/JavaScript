786554bff4c6f68b474ef905056c3d04eeb2bc37
EvertonDevsz <154710084+EvertonDevsz@users.noreply.github.com>
2026-04-01 08:26:10 -0300

    Entendendo estruturas de repetição

---
+++ MóduloE/Exercícios/Ex013/while.js
@@ -0,0 +1,11 @@
+let c = 1
+
+while(c <= 10){
+    console.log(`passo ${c}`) //teste lógico no início
+    c++ 
+}
+
+do{
+    console.log(`passo ${c}`) //teste lógico no final
+    c++ 
+} while(c <= 10)
